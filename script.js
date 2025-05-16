a873775b74dbbce2516c70b8b6ec4b8c0b9a38a8
Shaurya Pratap Singh <shaurya27pratapsingh@gmail.com>
2025-05-16 11:34:59 +0000

    Animating effect added

--- script.js
+++ script.js
@@ -11,28 +11,54 @@ function init(){
 }
 
 function play(){
-    bubbleSort(array);
-    showBars();
+    const copy=[...array];
+    const moves=bubbleSort(copy);
+    animate(moves);
 }
 
+function animate(moves){
+    if(moves.length==0){
+        showBars();
+        return;
+    }
+    const move = moves.shift();
+    const [i,j]=move.indices;
+    if(move.type=="swap"){
+    [array[i],array[j]]=[array[j],array[i]];
+    }
+    showBars(move);
+    setTimeout(function(){
+        animate(moves);
+    },200);
+}
+
+
 function bubblesort(array){
+    const moves=[];
     do{
         var swapped=false;
         for(let i=1;i<array.length;i++){
+            moves.push({indices:[i-1,i],type:"comp"});
             if(array[i-1]>[i]){
                 swapped=true;
+                moves.push({indices:[i-1,i],type:"swap"});
                 [array[i-1],array[i]]=[array[i],array[i-1]];
             }
         }
     }while(swapped);
+    return moves;
 }
 
-function showBars(){
+function showBars(move){
     container.innerHTML="";
 for(let i=0;i<array.length;i++){
     const bar=document.createElement("div");
     bar.style.height=array[i]*100+"%";
     bar.classList.add("bar");
-    Container.appendChild(bar);
+    if(move && move.indices.includes(i)){
+        bar.style.backgroundColor=
+        move.type=="swap"?"red":"blue";
+    }
+    container.appendChild(bar);
 }
 }
